db40bc4e012840de0fb588e1f3c9897f271ac6ba
alexmia <ale56.mia@gmail.com>
2020-05-08 11:35:50 +0200

    Basic help added

---
+++ help.py
@@ -0,0 +1,7 @@
+def printHelp():
+    print('Usage: python main.py [option] [args]\n')
+    print('Options\n')
+    print('Flag\t\tDescription')
+    print('-h\t\tShows this help dialog - Same as --help')
+    print('-l\t\tShows directly the loading')
+    print('-m\t\tShows the menu - Default if no option is set')
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from colored import fg, bg, attr
 import os
+import sys
 import time
 from loading import loading
 from auxf import del_last_lines, clear
+from help import printHelp
 
 def menu():
     print('%s1.%s Loading example' % (fg('red'), attr('reset')))
@@ -14,7 +16,22 @@ def menu():
     if choice == 1:
         loading()
 
+def argflags():
+    args = sys.argv[1:]
+    if len(args) == 0:
+        menu()
+    elif len(args) == 1:
+        if args[0] == '-h' or args[0]=='--help':
+            printHelp()
+        elif args[0] == '-l':
+            loading()
+        elif args[0] == '-m':
+            menu()
+        else:
+            print('Invalid option')
+            printHelp()
 
 
-clear()
-menu()
+if __name__ == "__main__":
+    clear()
+    argflags()
